11da9d3a4761bf8147ca99d5d85d7bef6421f7a2
likaiyuan559 <510311941@qq.com>
2019-06-09 19:04:39 +0800

    2

--- toucheffectslib/src/main/java/com/lky/toucheffectsmodule/factory/TouchEffectsFactory.java
+++ toucheffectslib/src/main/java/com/lky/toucheffectsmodule/factory/TouchEffectsFactory.java
@@ -6,6 +6,6 @@ import android.support.v4.view.LayoutInflaterCompat;
 
 public class TouchEffectsFactory {
     public static void initTouchEffects(@NonNull Activity activity){
-        LayoutInflaterCompat.setFactory2(activity.getLayoutInflater(),new TouchEffectsInflaterFactory());
+        LayoutInflaterCompat.setFactory(activity.getLayoutInflater(),new TouchEffectsInflaterFactory());
     }
 }
--- toucheffectslib/src/main/java/com/lky/toucheffectsmodule/factory/TouchEffectsInflaterFactory.java
+++ toucheffectslib/src/main/java/com/lky/toucheffectsmodule/factory/TouchEffectsInflaterFactory.java
@@ -2,23 +2,18 @@ package com.lky.toucheffectsmodule.factory;
 
 
 import android.content.Context;
+import android.support.v4.view.LayoutInflaterFactory;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.lky.toucheffectsmodule.TouchEffectsManager;
 
-public class TouchEffectsInflaterFactory implements LayoutInflater.Factory2 {
+public class TouchEffectsInflaterFactory implements LayoutInflaterFactory {
 
-
-    @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        return onCreateView(null,name,context,attrs);
-    }
 
     @Override
     public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
         return TouchEffectsManager.getViewSubject().createView(parent,name,context,attrs);
     }
-
 }
